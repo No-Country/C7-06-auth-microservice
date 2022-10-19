4f484a2aab558b0855ae1b4b214f9a994cf54bbc
Alberto Gómez Juan <agomezjuan@hotmail.com>
2022-10-19 16:22:49 +0200

    fix: parseInt id from params

--- controllers/User.js
+++ controllers/User.js
@@ -18,7 +18,7 @@ const getAllUsers = async (req, res) => {
 
 // update user route
 const updateUser = async (req, res) => {
-  if (req.auth.userId !== req.params.id) {
+  if (req.auth.userId != req.params.id) {
     return res.status(401).json({
       message: 'Auth failed.'
     });
@@ -58,7 +58,7 @@ const updateUser = async (req, res) => {
 
 // delete user route
 const deleteUser = async (req, res) => {
-  if (req.auth.userId !== req.params.id) {
+  if (req.auth.userId != req.params.id) {
     return res.status(401).json({
       message: 'Auth failed.'
     });
@@ -87,6 +87,62 @@ const deleteUser = async (req, res) => {
   }
 };
 
+// update user password route
+const updateUserPassword = async (req, res) => {
+  if (req.auth.userId != req.params.id) {
+    return res.status(401).json({
+      message: 'Auth failed.'
+    });
+  }
+  try {
+    const user = await User.findOne({
+      where: {
+        id: parseInt(req.params.id)
+      }
+    });
+    if (user) {
+      bcrypt.compare(req.body.password, user.password, (err, result) => {
+        if (err) {
+          return res.status(401).json({
+            message: 'Auth failed'
+          });
+        }
+        if (result) {
+          bcrypt.hash(req.body.newPassword, 10, async (err, hash) => {
+            if (err) {
+              return res.status(500).json({
+                error: err
+              });
+            } else {
+              try {
+                const updatedUser = await user.update({ password: hash });
+                res.status(200).json({
+                  message: 'User password updated',
+                  user: updatedUser
+                });
+              } catch (error) {
+                res.status(500).json({
+                  status: 'error',
+                  message: 'Internal server error'
+                });
+              }
+            }
+          });
+        }
+      });
+    } else {
+      res.status(404).json({
+        message: 'User not found'
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      status: 'error',
+      message: 'Internal server error'
+    });
+  }
+};
+
 // get user route
 const getUser = async (req, res) => {
   try {
@@ -110,6 +166,7 @@ const getUser = async (req, res) => {
 // export routes
 module.exports = {
   updateUser,
+  updateUserPassword,
   getAllUsers,
   deleteUser,
   getUser
